66c8b25f8c3879335a84f4e4628fbe05cc31f66d
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2024-04-10 11:08:17 +0200

    gjorde några ändringar till arena klassen

--- src/Arena.java
+++ src/Arena.java
@@ -5,28 +5,46 @@ public class Arena {
 
     public Arena(){
         Scanner scan = new Scanner(System.in);
-        Characters player = new Characters("Ben",100,10,10,10,10,10);
+        Knight player = new Knight("Ben",100,10,10,10,10,10);
         Characters enemy = new Characters("Dan",100,10,10,10,10,10);
         System.out.println("Welcome to the Arena!");
         boolean game = true;
         while (game){
             System.out.println("Type 1-2 to choose your next action.");
-            System.out.print("1. Fight\n2. Exit\nType here: ");
+            System.out.print("1. Fight\n2. Surrender\nType here: ");
             int choice = scan.nextInt();
             switch (choice){
                 case 1 -> {
-                    while(player.getHealth()>0 && enemy.getHealth()>0) {
-                        Random random = new Random();
-                        int value = random.nextInt(1, 6);
-                        player.dealDamage(enemy.getAttack(), value);
-                        enemy.dealDamage(player.getAttack(), value);
-                        player.currentMatch();
-                        enemy.currentMatch();
-                        if (player.getHealth() < enemy.getHealth()) {
-                            enemy.matchSummary();
+                    System.out.println("[Fight]\n1. Normal Attack\n2. Special Attack\n3. Go back");
+                    choice = scan.nextInt();
+                    switch (choice){
+                        case 1 -> {
+                            Random random = new Random();
+                            int value = random.nextInt(1, 6);
+                            player.dealDamage(enemy.getAttack(), value);
+                            enemy.dealDamage(player.getAttack(), value);
+                            player.currentMatch();
+                            enemy.currentMatch();
+                            if (player.getHealth() < enemy.getHealth()) {
+                                enemy.matchSummary();
+                            }
+                            if (enemy.getHealth() < player.getHealth()) {
+                                player.matchSummary();
+                            }
                         }
-                        if (enemy.getHealth() < player.getHealth()) {
-                            player.matchSummary();
+                        case 2 ->{
+                            Random random = new Random();
+                            int value = random.nextInt(1, 6);
+                            player.dealDamage(enemy.getSpecialAttack(), value);
+                            enemy.dealDamage(player.getSpecialAttack(), value);
+                            player.currentMatch();
+                            enemy.currentMatch();
+                            if (player.getHealth() < enemy.getHealth()) {
+                                enemy.matchSummary();
+                            }
+                            if (enemy.getHealth() < player.getHealth()) {
+                                player.matchSummary();
+                            }
                         }
                     }
                 }
--- src/Knight.java
+++ src/Knight.java
@@ -1,11 +1,4 @@
 public class Knight extends Characters{
-    private String name;
-    private int health;
-    private int attack;
-    private int defense;
-    private int specialAttack;
-    private int specialDefense;
-    private int speed;
     public Knight(String name, int health, int attack, int defense, int specialAttack, int specialDefense, int speed){
         super(name, health, attack, defense, specialAttack, specialDefense, speed);
     }
