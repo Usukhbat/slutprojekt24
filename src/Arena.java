dd765b632061c7160865428d0cce474400ee0e0c
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2024-04-22 09:43:40 +0200

    arbetade på slagsmål mekanismen och alla meddelanden som kommer upp.

--- src/Arena.java
+++ src/Arena.java
@@ -4,52 +4,47 @@ import java.util.Scanner;
 public class Arena {
 
     public Arena(){
+        Random random = new Random();
+        int value = random.nextInt(1, 6);
         Scanner scan = new Scanner(System.in);
         Knight player = new Knight("Ben",100,10,10,10,10,10);
         Characters enemy = new Characters("Dan",100,10,10,10,10,10);
         System.out.println("Welcome to the Arena!");
         boolean game = true;
         while (game){
-            System.out.println("Type 1-2 to choose your next action.");
-            System.out.print("1. Fight\n2. Surrender\nType here: ");
+            System.out.println("Write 1 or 2 to choose your next action.");
+            System.out.print("1. Fight\n2. Surrender\nWrite here: ");
             int choice = scan.nextInt();
             switch (choice){
                 case 1 -> {
-                    System.out.println("[Fight]\n1. Normal Attack\n2. Special Attack\n3. Go back");
-                    choice = scan.nextInt();
-                    switch (choice){
-                        case 1 -> {
-                            Random random = new Random();
-                            int value = random.nextInt(1, 6);
-                            player.dealDamage(enemy.getAttack(), value);
-                            enemy.dealDamage(player.getAttack(), value);
-                            player.currentMatch();
-                            enemy.currentMatch();
-                            if (player.getHealth() < enemy.getHealth()) {
-                                enemy.matchSummary();
+                    while(player.getHealth() > 0 && enemy.getHealth() > 0) {
+                        System.out.println("[" + player.getName() + " VS " + enemy.getName() + "]\n1. Normal Attack\n2. Special Attack\n3. Go back");
+                        choice = scan.nextInt();
+                        int report = 0;
+                        switch (choice) {
+                            case 1 -> {
+                                enemy.dealDamage(player.getName(), player.getAttack(), value);
+                                player.currentMatch(player.getName(), player.getHealth(), enemy.getName(), enemy.getHealth());
+                                report = enemy.winCondition(player.getHealth(), enemy.getHealth());
+                                enemy.matchEnd(report);
                             }
-                            if (enemy.getHealth() < player.getHealth()) {
-                                player.matchSummary();
+                            case 2 -> {
+                                enemy.dealDamage(player.getName(), player.getSpecialAttack(), value);
+                                player.currentMatch(player.getName(), player.getHealth(), enemy.getName(), enemy.getHealth());
+                                report = enemy.winCondition(player.getHealth(), enemy.getHealth());
+                                enemy.matchEnd(report);
                             }
-                        }
-                        case 2 ->{
-                            Random random = new Random();
-                            int value = random.nextInt(1, 6);
-                            player.dealDamage(enemy.getSpecialAttack(), value);
-                            enemy.dealDamage(player.getSpecialAttack(), value);
-                            player.currentMatch();
-                            enemy.currentMatch();
-                            if (player.getHealth() < enemy.getHealth()) {
-                                enemy.matchSummary();
-                            }
-                            if (enemy.getHealth() < player.getHealth()) {
-                                player.matchSummary();
+                            default -> {
+                                player.dealDamage(enemy.getName(), enemy.getSpecialAttack(), value);
+                                player.currentMatch(player.getName(), player.getHealth(), enemy.getName(), enemy.getHealth());
+                                report = player.winCondition(player.getHealth(), enemy.getHealth());
+                                player.matchEnd(report);
                             }
                         }
                     }
                 }
                 case 2 -> {
-                    System.out.println("Bye!");
+                    System.out.println("Embarrassing!");
                     game = false;
                 }
             }
--- src/Characters.java
+++ src/Characters.java
@@ -3,11 +3,11 @@ import java.util.Random;
 public class Characters {
     private final String name;
     private int health;
-    private int attack;
-    private int defense;
-    private int specialAttack;
-    private int specialDefense;
-    private int speed;
+    private final int attack;
+    private final int defense;
+    private final int specialAttack;
+    private final int specialDefense;
+    private final int speed;
     public Characters(String name, int health, int attack, int defense, int specialAttack, int specialDefense, int speed){
         this.name = name;
         this.health = health;
@@ -41,19 +41,33 @@ public class Characters {
         System.out.println("["+this.name+"]");
         System.out.println("Health: "+this.health);
         System.out.println("Attack: "+this.attack);
+        System.out.println("Defense: "+this.defense);
         System.out.println("Special Attack: "+this.specialAttack);
         System.out.println("Special Defense: "+this.specialDefense);
         System.out.println("Speed: "+this.speed);
     }
-    public void dealDamage(int damage, int value){
+    public void dealDamage(String name, int damage, int value){
         int totalDamage = damage+value;
         this.health = this.health-totalDamage;
-        System.out.println(this.health);
+        System.out.println(name+" dealt -"+totalDamage);
     }
-    public void currentMatch(){
-        System.out.println(this.name+": "+this.health);
+    public void currentMatch(String playerName, int playerHealth, String enemyName, int enemyHealth){
+        System.out.println(playerName+": "+playerHealth);
+        System.out.println(enemyName+": "+enemyHealth);
     }
-    public void matchSummary(){
-        System.out.println(this.name+" won!");
+    public void matchEnd(int report){
+        if (report == 1 || report == 2) {
+            System.out.println(this.name + " won!");
+        }
+    }
+    public int winCondition(int playerHealth, int enemyHealth) {
+        int message = 0;
+        if (playerHealth <= 0 && enemyHealth >= 0) {
+            message = 1;
+        }
+        if (playerHealth >= 0 && enemyHealth <= 0){
+            message = 2;
+        }
+        return message;
     }
 }
