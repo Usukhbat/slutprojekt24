3a01a50c8a259a9d91b60ffe3cb982e631891331
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2024-05-22 11:04:53 +0200

    fixade upp koden och nu har jag inga errors

--- src/Arena.java
+++ src/Arena.java
@@ -2,97 +2,126 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Arena {
+
     Characters player = new Characters(null, 100, 10, 10, 10, 10);
     static Classes classes = new Classes();
-    public Arena() {
+
+    public Arena() { // metoden där man kan välja sin klass.
         Scanner scan = new Scanner(System.in);
         try {
-            int choice = scan.nextInt();
-            System.out.print("Your name: ");
-            String name = scan.next();
-            switch (choice) {
-                case 1 -> player.classChoice(name, 12, 15, 8, 5);
-                case 2 -> player.classChoice(name, 15, 5, 12, 8);
-                case 3 -> player.classChoice(name, 5, 4, 16, 15);
-                case 4 -> player.classChoice(name, 15, 5, 15, 5);
-            }
-            boolean game = true;
+            playerChoice();
+            boolean game = true; // använder variabeln game som en on/off knapp för spelet
             while (game) {
-                System.out.println("Start? (Y/N)");
-                String choice = scan.nextLine();
-                switch (choice) {
-                    case "Y" -> {
-                        System.out.print("""
-                        Choose your enemy! (Write 1 - 4 to choose.)
-                        1. Karl the Knight
-                        2. Aaron the Archer
-                        3. Markus the Mage
-                        4. Robert the Rogue
-                        Write here:""");
-                        int enemyChoice = scan.nextInt();
-                        boolean fight = true; // Använder boolean värde för att styra koden. Starta det eller sluta det.
-                        while (fight) {      // Den här while loopen håller spelet.
-                            enemyChoice--;  // minus 1 för att välja från indexen, det börjar på 0 ju.
-                            fight = fightMenu(classes.enemies, enemyChoice);
-                        }
-                    }
-                    case "N" -> {
-                        game = false;
-                    }
-                }
+                game = enemyChoice(); // ropar en metod för att välja fienden.
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // simpel catch meddelande
             System.out.println("Try again, choose one of the options listed above.");
             scan.nextLine();
         }
     }
 
-    public boolean fightMenu(HashMap<Integer, Characters> classes, int enemyChoice) {
+    public void playerChoice() {
         Scanner scan = new Scanner(System.in);
-        while (player.getHealth() > 0 && classes.get(enemyChoice).getHealth() > 0) { // all the codes before the while loop is the 'main menu'/'lobby' of the game, will expand upon it further
-            System.out.println("[" + player.getName() + " VS " + classes.get(enemyChoice).getName() + "]");
-            // Använder speciell text kod för att skriva text.
-            System.out.print(""" 
-                                Write 1, 2, 3 or 4 to choose your next action.
-                                1. Normal Attack
-                                2. Special Attack
-                                3. Show info
-                                4. Surrender
-                                Write here:""");
-            int choice = scan.nextInt();
-            switch (choice) {          // uses an enhanced switch case to maneuver and do tasks around the program with inputs from Scanner.
-                case 1 -> normalAttack(classes, enemyChoice); // case för att köra normalAttack
-                case 2 -> specialAttack(classes, enemyChoice);
-                case 3 -> { // visa information för spelaren och fienden.
-                    player.showStats();
-                    classes.get(enemyChoice).showStats();
+        int choice = scan.nextInt();
+        System.out.print("Choose your name: ");
+        String name = scan.next();
+        switch (choice) { // beroende vilken klass man väljer så får man olika attributes för player
+            case 1 -> player.classChoice(name, 12, 15, 8, 5);
+            case 2 -> player.classChoice(name, 15, 5, 12, 8);
+            case 3 -> player.classChoice(name, 5, 4, 16, 15);
+            case 4 -> player.classChoice(name, 15, 5, 15, 5);
+        }
+    }
+    public boolean enemyChoice() { // Den här metoden låter dig välja din fiende. Om man väljer N så stoppas spelet och skickar tillbaka false till game.
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("Start? (Y/N)");
+            String chai = scan.nextLine();
+            switch (chai) {
+                case "Y" -> {
+                    System.out.print("""
+                            Choose your enemy! (Write 1 - 4 to choose.)
+                            1. Karl the Knight
+                            2. Aaron the Archer
+                            3. Markus the Mage
+                            4. Robert the Rogue
+                            Write here:""");
+                    int enemyChoice = scan.nextInt();
+                    boolean fight = true; // använder fight för att styra slagsmålet, ifall man ville ge upp halvvägs alltså.
+                    while (fight) {
+                        enemyChoice--;
+                        fight = fightMenu(classes.enemies, enemyChoice); // ifall man ger upp så skickas false till fight och man får välja sin fiende igen.
+                    }
                 }
-                case 4 -> { // Case för att ge upp. Det gör också så att liv för båda blir 100 igen.
-                    System.out.println("Goodbye!");
-                    player.reset();
-                    classes.get(enemyChoice).reset();
+                case "N" -> {
                     return false;
                 }
             }
+        }catch(Exception e){ // simpel catch meddelande
+            System.out.println("Try again, choose one of the options listed above.");
         }
-        if (player.getHealth() <= 0) {
-            System.out.println("You lost.");
-        }
-        if (classes.get(enemyChoice).getHealth() <= 0) {
-            System.out.println("You won!");
+        return true;
+    }
+
+    public boolean fightMenu(HashMap<Integer, Characters> classes, int enemyChoice) { // Den här metoden har switch case för att styra slagsmålet. Välj mellan 4 val för att styra.
+        Scanner scan = new Scanner(System.in);
+        try {
+            while (player.getHealth() > 0 && classes.get(enemyChoice).getHealth() > 0) {
+                System.out.println("\n[" + player.getName() + " VS " + classes.get(enemyChoice).getName() + "]");
+                System.out.print(""" 
+                        Write 1, 2, 3 or 4 to choose your next action.
+                        1. Normal Attack
+                        2. Special Attack
+                        3. Show info
+                        4. Surrender
+                        Write here:""");
+                int choice = scan.nextInt();
+                switch (choice) {
+                    case 1 -> normalAttack(classes, enemyChoice); // case för att köra normalAttack
+                    case 2 -> specialAttack(classes, enemyChoice); // case för att köra special attack
+                    case 3 -> showStats(classes, enemyChoice); // visa information för spelaren och fienden.
+                    case 4 -> {
+                        giveUp(classes, enemyChoice);
+                        return false; // ge upp och skicka false till variabeln fight
+                    }
+                }
+            }
+            winCondition(classes, enemyChoice);
+        }catch (Exception e){
+            System.out.println("Try again, choose one of the options listed above.");
         }
         return false;
     }
 
-    public void normalAttack(HashMap<Integer, Characters> classes, int enemyChoice){
-        player.warCry();
-        classes.get(enemyChoice).fight(player.getAttack(), player.getDefense(), player.getName());
-        System.out.println("(" + player.getName() + ": " + player.getHealth() + ")\n(" + classes.get(enemyChoice).getName() + ": " + classes.get(enemyChoice).getHealth() + ")");
-        classes.get(enemyChoice).warCry();
-        player.fight(classes.get(enemyChoice).getAttack(), classes.get(enemyChoice).getDefense(), classes.get(enemyChoice).getName());
+    public void showStats(HashMap<Integer, Characters> classes, int enemyChoice) { // metod för att visa stats för spelare och fiende
+        player.showStats();
+        classes.get(enemyChoice).showStats();
     }
 
-    public void specialAttack(HashMap<Integer, Characters> classes, int enemyChoice) {  // case för att köra specialAttack.
+    public void giveUp(HashMap<Integer, Characters> classes, int enemyChoice) { // Metoden för att ge spelare och fiende 100 och gå tillbaka till fiende menyn.
+        System.out.println("Goodbye!");
+        player.reset(); // använder this.health för att ge spelare och fiende 100 hälsa igen.
+        classes.get(enemyChoice).reset();
+    }
+
+    public void winCondition(HashMap<Integer, Characters> classes, int enemyChoice) { // kollar hälsan av båda för att se om någon vann.
+        if (player.getHealth() <= 0) { // om spelaren hälsa är noll eller mindre, då du förlorat
+            System.out.println("You lost.");
+        }
+        if (classes.get(enemyChoice).getHealth() <= 0) { // om fiendens hälsa är noll eller mindre, då du vann :)
+            System.out.println("You won!");
+        }
+    }
+
+    public void normalAttack(HashMap<Integer, Characters> classes, int enemyChoice){ // den här metoden hanterar normala attacker av båda
+        player.warCry(); // utrop av spelaren
+        classes.get(enemyChoice).fight(player.getAttack(), player.getDefense(), player.getName()); // Skickar spelaren attack, defense och namn till fight metoden i Characters för att skada fienden. Vi får fiendens hälsa för att metoden ropas med classes.get(enemyChoice)
+        System.out.println("\n(" + player.getName() + ": " + player.getHealth() + ")\n(" + classes.get(enemyChoice).getName() + ": " + classes.get(enemyChoice).getHealth() + ")");
+        classes.get(enemyChoice).warCry(); // utrop av fienden
+        player.fight(classes.get(enemyChoice).getAttack(), classes.get(enemyChoice).getDefense(), classes.get(enemyChoice).getName()); // samma fight metod som tidigare fast nu vi skadar spelaren och tar fiendens attributes.
+    }
+
+    public void specialAttack(HashMap<Integer, Characters> classes, int enemyChoice) { // Den här metoden hanterar special attack metoderna. Den är exakt likadan som normalAttack, enda skillnad är att det är med special attributes.
         player.warCry();
         classes.get(enemyChoice).fight(player.getSpecialAttack(), player.getSpecialDefense(), player.getName());
         System.out.println("(" + player.getName() + ": " + player.getHealth() + ")\n(" + classes.get(enemyChoice).getName() + ": " + classes.get(enemyChoice).getHealth() + ")");
--- src/Characters.java
+++ src/Characters.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Characters {        // this is the class where I have most of the important methods connected to the objects in Arena.java.
     private  String name;
-    private int health, attack, defense, specialAttack, specialDefense;
+    private int health, attack, defense, specialAttack, specialDefense; // alla attribute av en objekt
     public Characters(String name, int health, int attack, int defense, int specialAttack, int specialDefense){
         this.name = name;
         this.health = health;
@@ -26,31 +26,31 @@ public class Characters {        // this is the class where I have most of the i
         return specialDefense;
     }
     public void warCry(){
-        System.out.println("I shall not falter!");
-    }
-    public void classChoice(String name, int attack, int defense, int specialAttack, int specialDefense){
+        System.out.println("\nI shall not falter!");
+    } // Voice line for player before attack.
+    public void classChoice(String name, int attack, int defense, int specialAttack, int specialDefense){ // method for helping the player choose a class
         this.name = name;
         this.attack = attack;
         this.defense = defense;
         this.specialAttack = specialAttack;
         this.specialDefense = specialDefense;
     }
-    public void showStats(){                    // a simple method using abusing "this" to show the stats (attributes) of an object.
-        System.out.println("["+this.name+"]");
+    public void showStats(){  // a simple method using abusing "this" to show the stats (attributes) of an object.
+        System.out.println("\n["+this.name+"]");
         System.out.println("Health: "+this.health);
         System.out.println("Attack: "+this.attack);
         System.out.println("Defense: "+this.defense);
         System.out.println("Special Attack: "+this.specialAttack);
         System.out.println("Special Defense: "+this.specialDefense);
     }
-    public void fight(int damage, int defense, String name){
+    public void fight(int damage, int defense, String name){ // the main damage system
         Random rand = new Random();
         int random = rand.nextInt(1,6);
         damage = (damage+defense+random)/2;
         System.out.println(name +" dealt "+damage+"Hp!");
         this.health -= damage;
     }
-    public void reset(){
+    public void reset(){ // resets the health of the caller to 100.
         this.health = 100;
     }
 }
